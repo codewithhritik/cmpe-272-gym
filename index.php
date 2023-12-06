feddfc8803141f1eb9296006c2f0669142ea9f5c
Hrithik Bagane <hritikbagane@gmail.com>
2023-12-06 15:28:23 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+?>
+
 <?php include 'pageTracking.php'; ?>
 
 <!DOCTYPE html>
